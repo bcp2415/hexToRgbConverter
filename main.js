2bbd8090aefb33e46c483132390cbb87ab273c2f
bcp2415 <64213121+bcp2415@users.noreply.github.com>
2021-09-23 05:44:21 -0500

    adds console.log line to test import once we have an html file to load

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
 // file to practice importing functions from other modules, calling them, testing them...
+let convertHexToRgb = require("convertHexToRgb");
+
+console.log(convertHexToRgb("#07abdc"));
