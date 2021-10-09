7b359a0ae0488686364c55aa8f24f92bc74679a4
bcp2415 <64213121+bcp2415@users.noreply.github.com>
2021-10-09 17:00:27 -0500

    adds module for converting Rgb to Hex

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 // file to practice importing functions from other modules, calling them, testing them...
 import { convertHexToRgb } from "./modules/convertHexToRgb.js";
+import { convertRgbToHex } from "./modules/convertRgbToHex.js";
 
 console.log(convertHexToRgb("#0fddcc"));
+console.log(convertRgbToHex("rgb(100, 100, 100)"));
